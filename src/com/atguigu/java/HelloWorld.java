ae1c2f92c8a5abfc8bff327428156c27f6f5b8a7
浮伤&渲染流年 <278242987@qq.com>
2021-02-24 14:56:48 +0800

    尝试提交

--- src/com/atguigu/java/HelloWorld.java
+++ src/com/atguigu/java/HelloWorld.java
@@ -3,9 +3,6 @@ package com.atguigu.java;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello world!");
-        System.out.println("hello world!");
-        System.out.println("hello world!");
-
     }
 }
 
